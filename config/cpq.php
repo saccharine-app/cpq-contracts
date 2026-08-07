896acd2e50422ccb160ee86f66cc81d6284a15ed
Matthew Wegner <mwegner@proton.me>
2026-08-06 21:00:58 -0400

    add composer.json and config file

---
+++ config/cpq.php
@@ -0,0 +1,13 @@
+c<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Currency
+    |--------------------------------------------------------------------------
+    |
+    | The default currency used for catalog items and contract line items.
+    | 
+    */
+    'currency' => 'CAD',
+];
