5facef1dd96cfae6b9885c2647558faf75aeaf89
Matthew Wegner <mwegner@proton.me>
2026-08-14 12:45:31 -0400

    update DemoContext

--- database/migrations/0001_01_01_000005_create_demo_contexts_table.php
+++ database/migrations/0001_01_01_000005_create_demo_contexts_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('demo_contexts', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name');
             $table->timestamps();
         });
     }
--- src/Models/DemoContext.php
+++ src/Models/DemoContext.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class DemoContext extends Model
 {
-    //
+    use HasUuids;
+
+    protected $table = 'demo_contexts';
+
+    protected $fillable = [
+        'name',
+    ];
 }
